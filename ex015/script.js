15cc116ac570d61dcd1020adf48fb47a86f5ec68
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2021-11-17 15:59:50 -0300

    update_formulario

---
+++ ex015/script.js
@@ -0,0 +1,66 @@
+function verificar(){
+   var data = new Date()
+   var ano = data.getFullYear()
+   var fano = document.getElementById('txtano')
+   var res =document.querySelector('div#res')
+
+   if(fano.value.length == 0 || Number(fano.value) > ano){
+       window.alert('[ERRO] Verifique os dados e tente novamente! ')
+   }else{
+       var fsex = document.getElementsByName('radsex')
+       var idade = ano - Number(fano.value)
+       var genero = ''
+       var img = document.createElement('img')
+       img.setAttribute('id', 'foto')
+
+       if(fsex[0].checked){
+           genero = 'Homem'
+           if(idade >= 0 && idade < 10) {
+               //criança
+           }else if(idade < 21){
+               //jovem
+
+           }else if(idade < 65){
+               //adulto
+
+           }else{
+               //idoso
+
+           }
+
+       }else if(fsex[1].checked){
+           genero = 'Mulher'
+
+           if(idade >= 0 && idade < 10) {
+            //criança
+        }else if(idade < 21){
+            //jovem
+
+        }else if(idade < 65){
+            //adulto
+
+        }else{
+            //idoso
+
+        }
+
+       }else{
+           genero = 'Outros'
+           if(idade >= 0 && idade < 10) {
+            //criança
+        }else if(idade < 21){
+            //jovem
+
+        }else if(idade < 65){
+            //adulto
+
+        }else{
+            //idoso
+
+        }
+
+       }
+       res.style.textAlign = 'center'
+       res.innerHTML = ` Detectamos ${genero} com ${idade}`
+   }
+}
