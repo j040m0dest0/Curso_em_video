016b84be7ea11ba2e8ae2fd968f8c3286a1530af
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2021-11-18 10:52:07 -0300

    laços_repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,28 @@
+
+
+
+var c = 1
+do{
+    console.log(`Numero ${c}`)
+    c++ //c= c + 1
+}while(c <= 5)
+
+
+
+/*var c = 0
+while(c <= 500){
+    console.log(`Numero ${c}`)
+    c++ //c= c + 1
+}*/
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
---
+++ aula14/ambiente.js
@@ -0,0 +1,23 @@
+
+
+// var c = 1
+
+// while(c <= 5){
+//     console.log(`processo ${c}`)
+//     c++
+// }
+
+// var c = 1
+
+// do{
+//     console.log(`processo ${c}`)
+//     c++
+// }while(c <= 5)
+
+consolelog('Vai começar')
+
+for(var c = 1;c <= 5; c++){
+    console.log(`processo ${c}`)
+}
+
+console.log('FIM') 
