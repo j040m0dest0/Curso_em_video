b976907db9e879a279ec09c20c2565c03e14bbef
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2021-11-18 14:27:09 -0300

    second change in tabuada

--- aula14/ex017/script.js
+++ aula14/ex017/script.js
@@ -1,22 +1,32 @@
 function tabuada(){
     let num = document.querySelector('input#txtn')
     let tab = document.getElementById('seltab')
+    let res = document.querySelector('div#res')
 
     if(num.value.length == 0 ){
-        alert('Pro favor, digite um numero')
+        res.innerHTML = `Por favor insira um numero \u{1F620}`
     }else{
         let n = Number(num.value)
 
         // Realizando com while
 
-        let c = 1 
-        tab.innerHTML = ''
-        while(c <= 10){
+        // let c = 1 
+        // tab.innerHTML = ''
+        // while(c <= 10){
+        //     let item = document.createElement('option')
+        //     item.text = `${n} x ${c} = ${n*c}`
+        //     item.value = `tab${c}`
+        //     tab.appendChild(item)
+        //     c++
+        // }
+
+        // Realizando usando FOR
+        tab.innerHTML = ' realizando a tabuada'
+        for(let c = 1 ; c <= 10 ; c++ ){
             let item = document.createElement('option')
-            item.text = `${n} x ${c} = ${n*c}`
-            item.value = `tab${c}`
-            tab.appendChild(item)
-            c++
+                item.text = `${n} x ${c} = ${n*c}`
+                 //item.value = `tab${c}`
+                 tab.appendChild(item)
         }
 
     }
