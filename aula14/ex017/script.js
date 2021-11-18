9155b02b1394a95f8ae15db5cf43d56b27ae87a6
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2021-11-18 14:14:50 -0300

    Tabuada

--- aula14/ex017/script.js
+++ aula14/ex017/script.js
@@ -1,1 +1,23 @@
-alert('hello')
+function tabuada(){
+    let num = document.querySelector('input#txtn')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0 ){
+        alert('Pro favor, digite um numero')
+    }else{
+        let n = Number(num.value)
+
+        // Realizando com while
+
+        let c = 1 
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+
+    }
+}
