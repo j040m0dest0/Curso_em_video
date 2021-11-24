e49268ca5c88cf41f2ca5292817a4c742fff38ce
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2021-11-24 09:14:50 -0300

    update cadastro pessoa

---
+++ js_training/main.js
@@ -0,0 +1,17 @@
+
+function adicionar(){
+
+  let nome = document.querySelector('input#txtn')
+  let profissao = document.querySelector('input#txtp')
+
+}
+
+
+let human = {
+  nome:'',
+  sexo:'',
+  profissão :'',
+  idade: 0,
+  
+
+}
